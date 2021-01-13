0a9c2e9306f6764bb89b5911678d990baeb36628
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-01-13 15:02:39 +0100

    test met quote-generator

--- examples/inspireer.py
+++ examples/inspireer.py
@@ -2,64 +2,93 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+picdir = os.path.join(os.path.dirname(
+    os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(
+    os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
-
-import logging
-from waveshare_epd import epd2in13_V2
-import time
-from PIL import Image,ImageDraw,ImageFont
-import traceback
 import json
+import time
+from waveshare_epd import epd2in13_V2
+from datetime import datetime
+import random
+import logging
+from PIL import Image, ImageDraw, ImageFont
+import traceback
 
-logging.basicConfig(level=logging.DEBUG)
 
 
-try:
-    
+logging.basicConfig(level=logging.INFO)
+
+def itereer_over_lijst():
+    lijst = geef_een_lijst()
+    logging.info('Van start met een lijst van ' + str(len(lijst)) + ' quotes.')
+    i = 0
+    sec_wachten = 5
+    max_quotes_per_lijst = 10
+    for quote in lijst:
+        bouw_afbeelding_met_quote(quote)
+        i = i+1
+        time.sleep(sec_wachten)
+        if(i >= max_quotes_per_lijst):
+            logging.info('Er zijn ' + str(max_quotes_per_lijst) +
+                         ' quotes getoond, tijd voor een nieuwe lijst')
+            itereer_over_lijst()
+
+
+def geef_een_lijst():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    lijst_wat_te_tonen = []
+
     with open('../quotes.json') as jsonFile:
         quotes = json.load(jsonFile)
 
+    for i in quotes:
+        start_time = i['time']
+        end_time = i['end-time']
+        if(current_time > start_time and current_time < end_time):
+            # controle op dag-specifieke quotes toevoegen
+            quote = i['text']
+            lijst_wat_te_tonen.append(quote)
+
+    random.shuffle(lijst_wat_te_tonen)
+    return lijst_wat_te_tonen
+
+
+def bouw_afbeelding_met_quote(quote):
     epd = epd2in13_V2.EPD()
-    
-    for jsonItem in quotes:
-        mainQuote = jsonItem['text']
-        epd.init(epd.FULL_UPDATE)
-        epd.Clear(0xFF)
+    epd.init(epd.FULL_UPDATE)
+    epd.Clear(0xFF)
 
-        font14 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 14)
-        font17 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 17)
-        font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-        font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-        
-        image = Image.new('1', (epd.height, epd.width), 255)  # Frame eerst poetsen 
-        draw = ImageDraw.Draw(image)
+    font17 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 17)
 
-        # Breek zinnen af zodat het past op scherm
-        def wrap_by_word(tekst, linebreakLocatie):
-            gespletenTekst = tekst.split()
-            resultaat = ''
-            for i in range(0, len(gespletenTekst), linebreakLocatie):
-                resultaat += ' '.join(gespletenTekst[i:i+linebreakLocatie]) + '\n'
-
+    image = Image.new('1', (epd.height, epd.width), 255)  # Frame eerst poetsen
+    draw = ImageDraw.Draw(image)
+    logging.info('Afbeelding maken van de gevonden quote.')
+    # Breek zinnen af zodat het past op scherm
+    def wrap_by_word(tekst, linebreakLocatie):
+        gespletenTekst = tekst.split()
+        resultaat = ''
+        for i in range(0, len(gespletenTekst), linebreakLocatie):
+            resultaat += ' '.join(gespletenTekst[i:i +linebreakLocatie]) + '\n'
             return resultaat
-        
-        # Centrale quote
-        draw.multiline_text((10, 10), wrap_by_word(mainQuote, 4), align= "left", font = font17, fill = 0)
-        
-        epd.display(epd.getbuffer(image))
 
-        time.sleep(7200)
+        # Centrale quote
+        draw.multiline_text((10, 10), wrap_by_word(quote, 4), align="left", font=font17, fill=0)
+
+        epd.display(epd.getbuffer(image))
         epd.sleep()
-    
+
     epd.Dev_exit()
-        
-        
+
+try:
+    itereer_over_lijst
+
 except IOError as e:
     logging.info(e)
-    
+
 except KeyboardInterrupt:
     epd2in13_V2.epdconfig.module_exit()
     exit()
---
+++ examples/quote_generator.py
@@ -0,0 +1,47 @@
+import json
+import time
+import random
+import logging
+from datetime import datetime
+
+logging.basicConfig(level=logging.INFO)
+
+def itereer_over_lijst():
+    lijst = geef_een_lijst()
+    logging.info('Van start met een lijst van ' + str(len(lijst)) + ' quotes.')
+    i = 0
+    sec_wachten = 5
+    max_quotes_per_lijst = 10
+    for quote in lijst:
+        bouw_afbeelding_met_quote(quote)
+        i = i+1
+        time.sleep(sec_wachten)
+        if(i >= max_quotes_per_lijst):
+            logging.info('Er zijn ' + str(max_quotes_per_lijst) + ' quotes getoond, tijd voor een nieuwe lijst')
+            itereer_over_lijst()
+
+def geef_een_lijst():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    lijst_wat_te_tonen = []
+
+    with open('../quotes.json') as jsonFile:
+        quotes = json.load(jsonFile)
+
+    for i in quotes:
+        start_time = i['time']
+        end_time = i['end-time']
+        if(current_time > start_time and current_time < end_time):
+            # controle op dag-specifieke quotes toevoegen
+            quote = i['text']
+            lijst_wat_te_tonen.append(quote)
+
+    random.shuffle(lijst_wat_te_tonen)
+    return lijst_wat_te_tonen
+
+def bouw_afbeelding_met_quote(quote):
+    return quote
+    # drawen op het scherm (voor nu printen)
+
+if __name__ == '__main__':
+    itereer_over_lijst()
